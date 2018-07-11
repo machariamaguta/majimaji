13b8e4ecc7972716cb7e438a59105fca2a0f3629
hilpitome <hilpitome@gmail.com>
2018-07-11 17:45:44 +0300

    parsing fcm message

--- app/src/main/java/mfanyakazi/com/mobiwater/service/MyFirebaseMessagingService.java
+++ app/src/main/java/mfanyakazi/com/mobiwater/service/MyFirebaseMessagingService.java
@@ -7,10 +7,62 @@ import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.gson.Gson;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private final String TAG = MyFirebaseMessagingService.class.getSimpleName();
     @Override
     public void onMessageReceived(RemoteMessage message) {
-        System.out.print("firebase notification "+message.getNotification().getBody());
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + message.getFrom());
+
+
+
+        // Check if message contains a data payload.
+        if (message.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " +  message.getData().get("time"));
+
+            Gson gson = new Gson();
+//            DataObject dataObject = gson.fromJson(message.getData(), MyFirebaseMessagingService.DataObject.class);
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
+//                scheduleJob();
+            } else {
+                // Handle message within 10 seconds
+                handleNow();
+            }
+
+        }
+
+        // Check if message contains a notification payload.
+        if (message.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + message.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+
     }
+    public class DataObject{
+        @SerializedName("time")
+        @Expose
+        private String time;
+        public DataObject(){}
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time){
+            this.time = time;
+        }
+    }
+
+    public void handleNow(){}
 }
